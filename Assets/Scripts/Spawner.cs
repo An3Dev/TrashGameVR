8407a7eb16b500b7ca6cd13334ad8df3add9c621
nazeern <nitin.nazeer@gmail.com>
2022-10-23 18:56:29 -0700

    Create duplicate scene with changes

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    // How quickly to spawn prefabs
+    public float InitialSpawnDelay;
+
+    // How much to decrease the spawn delay each time
+    public float SpawnDelayDecrement;
+
+    // What is the fastest we spawn prefabs
+    public float MinSpawnDelay;
+
+    // How much time elapsed after spawn
+    private float elapsedTime;
+
+    // The current spawn delay
+    private float currSpawnDelay;
+
+    // The location to spawn prefabs at
+    public Transform spawnAnchor;
+
+    // The available prefabs to choose from
+    public GameObject[] availablePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+        currSpawnDelay = InitialSpawnDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= currSpawnDelay) {
+            elapsedTime = 0;
+            currSpawnDelay = Mathf.Max(currSpawnDelay - SpawnDelayDecrement, MinSpawnDelay);
+            InitRandom();
+        }
+        
+    }
+
+
+    void InitRandom() {
+        GameObject prefab = availablePrefabs[Random.Range(0, availablePrefabs.Length)];
+        Instantiate(prefab, spawnAnchor.position, Quaternion.identity);
+    }
+}
